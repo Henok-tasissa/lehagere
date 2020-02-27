8b7ce51c31b08bf50a30d58360757a48bc715342
Henok Tasissa <henokjimma@gmail.com>
2020-02-27 09:20:13 -0500

    Removed file

--- henok/projects/todoapp/php/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new Mysqli("localhost","henoktas_issa","Ethiopia1@","henoktas_todoapp");
-if($conn->connect_errno){
-    die("Connection Error");
-}
-?>
--- henok/projects/todoapp/php/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Credentials: true');
-?>
